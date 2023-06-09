4523ec2891851ec945978c22ec4262c455ad63c1
IndraAlFauzan <39118928+IndraAlFauzan@users.noreply.github.com>
2023-06-09 10:34:11 +0700

    create comment

--- circularlinkedlist/circularlinkedlist.cpp
+++ circularlinkedlist/circularlinkedlist.cpp
@@ -32,6 +32,7 @@ public:
 };
 
 void CircularLinkedList::addNode() {
+	// Function to add a node to the circular linked list
 
 	int rollNo;
 	string nm;
@@ -40,30 +41,38 @@ void CircularLinkedList::addNode() {
 	cout << "Enter the name of the student: ";
 	cin >> nm;
 
+	// Create a new node and assign the input values
 	Node* newnode = new Node();
 	newnode->rollNumber = rollNo;
 	newnode->name = nm;
 
+	// Check if the list is empty
 	if (LAST == NULL) {
+		// If the list is empty, make the new node the last node
 		newnode->next = LAST;
 		LAST = newnode;
-		LAST->next = LAST;
+		LAST->next = LAST;  // Point the last node back to itself
 		return;
 	}
+	// Check if the roll number of the new node is less than or equal to the roll number of the first node
 	else if (rollNo <= LAST->next->rollNumber) {
+		// If it is, check if the roll numbers match
 		if (rollNo == LAST->next->rollNumber) {
 			cout << "\nDuplicate roll numbers not allowed\n";
 			return;
 		}
+		// Insert the new node at the beginning of the list
 		newnode->next = LAST->next;
 		LAST->next = newnode;
 		return;
 	}
 
+	// Traverse the list to find the appropriate position to insert the new node
 	Node* previous = LAST->next;
 	Node* current = LAST->next;
 
 	while (rollNo >= current->rollNumber && current != LAST) {
+		// Check if the roll numbers match
 		if (rollNo == current->rollNumber) {
 			cout << "\nDuplicate roll numbers not allowed\n";
 			return;
@@ -72,16 +81,17 @@ void CircularLinkedList::addNode() {
 		current = current->next;
 	}
 
+	// Check if the new node should be inserted at the end of the list
 	if (previous == LAST) {
 		newnode->next = LAST->next;
 		LAST->next = newnode;
 		LAST = newnode;
 	}
 	else {
+		// Insert the new node between the previous and current node
 		newnode->next = current;
 		previous->next = newnode;
 	}
-
 }
 
 bool CircularLinkedList::search(int rollno, Node** previous, Node** current) {
@@ -125,12 +135,12 @@ bool CircularLinkedList::delNode() {
 	do {
 		if (current->rollNumber == rollNo) {
 			if (current == LAST->next && current == LAST) { // Only one node in the list
-				 current = NULL;
+				current = NULL;
 				LAST = NULL;
 			}
 			else if (current == LAST->next) { // Deleting the first node
 				LAST->next = current->next;
-				 current = NULL;
+				current = NULL;
 			}
 			else if (current == LAST) { // Deleting the last node
 				previous->next = current->next;
@@ -139,7 +149,7 @@ bool CircularLinkedList::delNode() {
 			}
 			else { // Deleting a node in between
 				previous->next = current->next;
-				 current = NULL;
+				current = NULL;
 			}
 
 			cout << "Record deleted successfully" << endl;
